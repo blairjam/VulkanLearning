bf1043171710d9f5831400b8f45942e112e749ee
Conner Blair <cblair1353@gmail.com>
2017-09-04 00:59:36 -0700

    Empty window created.

--- Triangle/headers/HelloTriangle.hpp
+++ Triangle/headers/HelloTriangle.hpp
@@ -1,10 +1,20 @@
 #pragma once
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 class HelloTriangle {
 public:
     void run();
 
 private:
+    const int WINDOW_WIDTH = 800;
+    const int WINDOW_HEIGHT = 600;
+    const char* WINDOW_TITLE = "Vulkan";
+    
+    GLFWwindow* window;
+
+    void initWindow();
     void initVulkan();
     void mainLoop();
     void cleanup();
--- Triangle/source/HelloTriangle.cpp
+++ Triangle/source/HelloTriangle.cpp
@@ -2,9 +2,22 @@
 
 void HelloTriangle::run()
 {
+    initWindow();
     initVulkan();
     mainLoop();
     cleanup();
+}
+
+void HelloTriangle::initWindow()
+{
+    glfwInit();
+
+    // Tell glfw not to create an OpenGL context.
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    // Disable window resizing.
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, nullptr, nullptr);
 }
 
 void HelloTriangle::initVulkan()
@@ -13,8 +26,14 @@ void HelloTriangle::initVulkan()
 
 void HelloTriangle::mainLoop()
 {
+    while (!glfwWindowShouldClose(window)) 
+    {
+        glfwPollEvents();
+    }
 }
 
 void HelloTriangle::cleanup()
 {
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
